aa7f396ffca945692d0b58ffc41f43bc800184e7
vasudevareddyreddem <vasudevareddy549@gmail.com>
2018-10-05 17:23:22 +0530

    adding
    adding

--- application/views/html/header.php
+++ application/views/html/header.php
@@ -5,9 +5,8 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, initial-scale=1" name="viewport" />
- 
-    <title>Hospital Agent</title>
-    <!-- google font -->
+	<title>Hospital Agent</title>
+	<!-- google font -->
      <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&amp;subset=all" rel="stylesheet" type="text/css" />
 	<!-- icons -->
     <link href="<?php echo base_url(); ?>assets/vendor/assets/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
